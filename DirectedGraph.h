1a8cf48b17815401a4e47336f55f5accf65fd6ab
Mridsole <mridsole@gmail.com>
2016-02-10 03:27:49 +1100

    finally fixed the emplace issue on structs with variadic template constructors

--- DirectedGraph.h
+++ DirectedGraph.h
@@ -1,39 +1,72 @@
-#pragma once
-
 #include <vector>
 #include <stdexcept>
 
+// debugging
+#include "stdio.h"
+
+// directed graph
 namespace graph {
 
-class DirectedGraph {
+template <typename EdgeT, typename NodeT>
+class DirectedGraph
+{
 
 public:
-	
+
 	// forward declarations
 	struct Edge;
 	struct Node;
-	struct Connection;
 
-	using Edges = std::vector<Edge*>;
-	using Nodes = std::vector<Node*>;
+	struct Connection
+	{
+		Edge* edge;
+		Node* node;
+		bool end;
+
+		Connection(Edge* _edge, Node* _node, bool _end) :
+			edge(_edge),
+			node(_node),
+			end(_end)
+		{};
+	};
+
 	using Connections = std::vector<Connection>;
+	using Edges = std::vector<Edge>;
+	using Nodes = std::vector<Node>;
 
+	// actual implementations
 	struct Edge
 	{
 		friend class DirectedGraph;
 
 		Node* startNode;
 		Node* endNode;
+		EdgeT data;
 
 		// can only be constructed by DirectedGraph::createEdge
-	protected:
+		//private:
 
-		Edge(Node* _startNode, Node* _endNode) :
+		template <typename... Ts>
+		Edge(Node* _startNode, Node* _endNode, Ts&&... args) :
 			startNode(_startNode),
-			endNode(_endNode)
+			endNode(_endNode),
+			data(args...)
 		{};
 
-		virtual ~Edge() {};
+		// copy constructors
+		Edge(Edge&& rhs) :
+			startNode(rhs.startNode),
+			endNode(rhs.endNode),
+			data(rhs.data)
+		{};
+
+		Edge(Edge& rhs) :
+			startNode(rhs.startNode),
+			endNode(rhs.endNode),
+			data(rhs.data)
+		{};
+
+		~Edge() {};
 	};
 
 	struct Node
@@ -41,172 +74,65 @@ public:
 		friend class DirectedGraph;
 
 		Connections connections;
+		NodeT data;
 
-	protected:
-		Node() : connections() {};
-		virtual ~Node() {};
-	};
-
-	struct Connection
-	{
-		friend class DirectedGraph;
-
-		Edge* edge;
-		Node* node;
-		bool end;
-
-		bool operator==(const Connection& rhs) {
-			return edge == rhs.edge && node == rhs.node && end == rhs.end;
-		}
-
-	protected:
-		Connection(Edge* _edge, Node* _node, bool _end) :
-			edge(_edge),
-			node(_node),
-			end(_end)
+		template <typename... Ts>
+		Node(Ts&&... args) :
+			data(args...)
 		{};
+
+		// copy constructors - need these to avoid obscure errors!
+		Node(Node&& rhs) :
+			connections(rhs.connections),
+			data(rhs.data)
+		{};
+
+		Node(Node& rhs) :
+			connections(rhs.connections),
+			data(rhs.data)
+		{};
+
+		~Node() {};
 	};
-	
+
 	// DirectedGraph stuff
 	DirectedGraph() {};
 	~DirectedGraph() {};
 
-	template <typename NodeType>
-	NodeType* createNode() {
+	template <typename... Ts>
+	Edge& createEdge(Node* _startNode, Node* _endNode, Ts&&... args) {
 
-		NodeType* node = new NodeType();
-		nodes.push_back(node);
-		return node;
+		if (_startNode == nullptr && _endNode == nullptr)
+			throw std::invalid_argument("Error creating edge: both node pointers cannot be null.");
+
+		// create the edge in place in our edges vector, then get a pointer to it
+		this->edges.emplace_back(_startNode, _endNode, args...);
+		Edge* edge = &(*(this->edges.end() - 1));
+
+		// give the pointer to our nodes
+		if (_startNode != nullptr)
+			_startNode->connections.emplace_back(edge, _startNode, false);
+
+		if (_endNode != nullptr)
+			_endNode->connections.emplace_back(edge, _endNode, false);
+
+		return *edge;
 	}
 
-	template <typename EdgeType>
-	EdgeType* createEdge(Node* startNode, Node* endNode) {
+	Node& createNode(NodeT args) {
 
-		if (startNode == nullptr && endNode == nullptr)
-			throw std::invalid_argument("Error creating edge: both nodes can't be null.");
+		this->nodes.emplace_back(args);
+		DirectedGraph<EdgeT, NodeT>::Node* node = &(*(this->nodes.end() - 1));
 
-		EdgeType* edge = new EdgeType(startNode, endNode);
-
-		if (startNode != nullptr)
-			startNode->connections.push_back(Connection(edge, startNode, false));
-
-		if (endNode != nullptr)
-			endNode->connections.push_back(Connection(edge, endNode, true));
-
-		edges.push_back(edge);
-		return edge;
-	}
-
-	template <typename NodeType, typename... Ts>
-	NodeType* emplaceNode(Ts&&... args) {
-
-		nodes.emplace_back(args...);
-		Node* node = &(*(nodes.end() - 1));
-		return node;
-	}
-
-	template <typename EdgeType, typename... Ts>
-	EdgeType* emplaceEdge(Node* startNode, Node* endNode, Ts&&... args) {
-
-		if (startNode == nullptr && endNode == nullptr)
-			throw std::invalid_argument("Error creating edge: both nodes can't be null.");
-
-		edges.emplace_back(args...);
-		EdgeType* edge = &(*(edges.end() - 1));
-
-		if (startNode != nullptr)
-			startNode->connections.push_back(Connection(edge, startNode, false));
-
-		if (endNode != nullptr)
-			endNode->connections.push_back(Connection(edge, endNode, true));
-		
-		return edge;
-	}
-
-	Connection* findConnection(Edge* edge, Node* node) {
-
-		for (auto it = node->connections.end() - 1; it >= node->connections.begin(); it--) {
-			if (it->edge == edge)
-				return &(*it);
-		}
-
-		return nullptr;
-	}
-
-	// remove an edge from the graph and delete it
-	void deleteEdge(Edge* edge) {
-
-		if (edge == nullptr)
-			return;
-
-		// remove connections from the connected nodes
-		Connection* connectionStart = findConnection(edge, edge->startNode);
-		Connection* connectionEnd = findConnection(edge, edge->endNode);
-
-		if (connectionStart != nullptr)
-			removeConnection(*connectionStart);
-
-		if (connectionEnd != nullptr)
-			removeConnection(*connectionEnd);
-
-		delete edge;
-	}
-
-	// remove a node from the graph and delete it
-	void deleteNode(Node* node) {
-
-		if (node == nullptr)
-			return;
-
-		// remove all the node's connections to edges
-		for (auto it = node->connections.end() - 1; it >= node->connections.begin(); it--) {
-			
-			if (it->end)
-				it->edge->endNode = nullptr;
-			else
-				it->edge->startNode = nullptr;
-		}
-
-		// delete the node
-		delete node;
-	}
-
-	// remove a connection from the graph
-	void removeConnection(Connection& connection) {
-
-		if (connection.node == nullptr || connection.edge == nullptr)
-			return;
-
-		if (connection.end)
-			connection.edge->endNode = nullptr;
-		else
-			connection.edge->startNode = nullptr;
-
-		Node* node = connection.node;
-		for (auto it = node->connections.end() - 1; it >= node->connections.begin(); it--) {
-			if (*it == connection) {
-				node->connections.erase(it);
-			}
-		}
-	}
-
-	// does every edge have two nodes on it?
-	bool isGraphClosed() {
-
-		for (auto it = edges.begin(); it < edges.end(); it++) {
-			if ((*it)->startNode == nullptr || (*it)->endNode == nullptr)
-				return false;
-		}
-
-		return true;
+		return *node;
 	}
 
 
-
-protected:
-
-	Edges edges;
+	// node centric - store a list of all the nodes
 	Nodes nodes;
+
+	// might as well store some edges too
+	Edges edges;
 };
 
 };
--- main.cpp
+++ main.cpp
@@ -4,16 +4,23 @@
 
 #include "DirectedGraph.h"
 
+#include <utility>
+#include <vector>
+
 using namespace hwgame;
 
+#include "stdio.h"
 
+using CircuitGraph = graph::DirectedGraph<float, int>;
+using CircuitEdge = CircuitGraph::Edge;
+using CircuitNode = CircuitGraph::Node;
 
 // program entry
 int main() {
 
-	//HeatWave* game = new HeatWave();
-	//game->start();
+	CircuitGraph* testGraph = new CircuitGraph();
 
-	// test graph-simulation interfacing
-
+	CircuitNode node1 = testGraph->createNode(1);
+	CircuitNode node2 = testGraph->createNode(5);
+	CircuitEdge edge1 = testGraph->createEdge(&node1, &node2, 2.1f);
 }
