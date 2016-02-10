4c5d92776020523f4887767c843da54926e1f70c
Mridsole <mridsole@gmail.com>
2016-02-11 01:15:18 +1100

    more testing infrastructure

---
+++ TestCircuitGraph.cpp
@@ -0,0 +1,9 @@
+#include "stdafx.h"
+
+#include "UnitTest++/UnitTest++.h"
+
+// well, for now let's just try a sanity test
+TEST(Sanity)
+{
+	CHECK_EQUAL(1, 2);
+}
--- main.cpp
+++ main.cpp
@@ -7,56 +7,11 @@
 #include <utility>
 #include <vector>
 
+#include "UnitTest++/UnitTest++.h"
 
-using namespace hwgame;
-
-#include "stdio.h"
-
-// funky forward decl stuff
-struct CircuitEdgeData;
-struct CircuitVertexData;
-
-using CircuitEdge = dirgraph::Edge<CircuitEdgeData, CircuitVertexData>;
-using CircuitVertex = dirgraph::Vertex<CircuitEdgeData, CircuitVertexData>;
-using CircuitEdgeDataBase = dirgraph::EdgeData<CircuitEdgeData, CircuitVertexData>;
-using CircuitVertexDataBase = dirgraph::VertexData<CircuitEdgeData, CircuitVertexData>;
-
-struct CircuitEdgeData : public CircuitEdgeDataBase
-{
-	float x;
-	float y;
-
-	CircuitEdgeData(CircuitEdge* _thisEdge, float _x, float _y) :
-		CircuitEdgeDataBase::EdgeData(_thisEdge),
-		x(_x), y(_y)
-	{};
-};
-
-struct CircuitVertexData : public CircuitVertexDataBase
-{
-	float z;
-
-	CircuitVertexData(CircuitVertex* _thisVertex, float _z) :
-		CircuitVertexDataBase::VertexData(_thisVertex),
-		z(_z)
-	{};
-};
 
 // program entry
 int main() {
 
-	// make some nodes!
-	CircuitVertex* node1 = new CircuitVertex(10.f);
-	CircuitVertex* node2 = new CircuitVertex(21.5f);
-
-	// get the data of node 1
-	auto& node1data = node1->data;
-
-	// do some things ...
-
-	// maybe we don't have node1 anymore - how to get it back?
-	CircuitVertex* node1again = node1data.vertex;
-
-	// this is why we did all that template mess above - to facilitate a two-way
-	// relationship between the node and it's data type. 
+	UnitTest::RunAllTests();
 }
